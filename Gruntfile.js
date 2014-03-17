21cad320ce9fd21ff247cdef3f4449e369615328
Brian Bouril <brian@brianbouril.com>
2014-03-16 23:46:08 -0500

    added bower_components and node_components to .gitignore, added connect and watch to allow grunt command with livereload on change, added jshint, added examples directory for individual example pages, added src directory for core library functionality

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -11,13 +12,59 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+
+    jshint: {
+      options: {
+        "curly": true,
+        "eqnull": true,
+        "eqeqeq": true,
+        "undef": true,
+        "unused": true,
+        "latedef": true,
+        "camelcase": true,
+        "strict": false,
+        "newcap": true,
+        "maxcomplexity": 3,
+        "globals": {}
+      },
+      all: ['src/*.js']
+    },
+
+    connect: {
+      livereload: {
+        options: {
+          port: 9000,
+          hostname: 'localhost',
+          base: '.',
+          open: true,
+          livereload: true
+        }
+      }
+    },
+
+    watch: {
+      livereload: {
+        options: {
+          livereload: true
+        },
+        files: ['examples/*.html', 'src/*.js', 'src/*.css']
+      },
+      jshint: {
+        files: ['src/*.js'],
+        tasks: ['jshint']
+      }
     }
+
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['connect:livereload', 'watch']);
 
 };
---
+++ src/barChartBasic.js
@@ -0,0 +1,5 @@
+(function(){
+
+  // Start the fun here...
+
+})();
